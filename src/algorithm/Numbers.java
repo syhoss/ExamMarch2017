76aa3e11f1fe8ac32bd770864cc13460ddec30e3
matiur rahman <mrahman@fuzzproductions.com>
2017-04-23 11:30:08 -0400

    midterm

---
+++ src/algorithm/Numbers.java
@@ -0,0 +1,42 @@
+package algorithm;
+import java.util.Random;
+
+/*
+ *Created by PIIT_NYA on 04/22/2017.
+ */
+public class Numbers {
+
+	/*
+	 * Show all the different kind of sorting algorithm by applying into (num array).
+	 * Display the execution time for each sorting.Example in below.
+	 *
+	 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
+	 */
+
+	public static void main(String[] args) {
+		
+		int [] num = new int[10000];
+		
+		Random rand = new Random();
+		for(int i=0; i<num.length; i++){
+			
+		num[i] = rand.nextInt(1000000);
+			
+		}
+
+		//Selection Sort
+		Sort algo = new Sort();
+		algo.selectionSort(num);
+		long selectionSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
+
+		//Insertion Sort
+		algo.insertionSort(num);
+		long insertionSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+
+		//Continue for rest of the Sorting Algorithm....
+
+	}
+
+}
